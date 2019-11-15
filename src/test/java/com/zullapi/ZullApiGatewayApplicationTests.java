d393a58211f555ca510492a8c1a1458802ec205b
Nagendra Kumar Patel <nagendra.patel@tavant.com>
2019-11-15 09:40:34 +0530

    first commit

---
+++ src/test/java/com/zullapi/ZullApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
